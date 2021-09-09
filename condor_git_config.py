9f9f1e6a41b08c34c254aed4e96dece483f19c7a
Max Fischer <maxfischer2781@gmail.com>
2021-09-09 16:45:17 +0200

    Cleanup and Updates (#8)
    * switched to flit
    
    * automatic black and lint
    
    * max age is a float (including inf)
    
    * cache lock is stored alongside cache
    
    * using pathlib instead of raw path strings
    
    * metadata is handled as transaction
    
    * tracking pulls/reads
    
    * cache stats are reported as config comment
    
    * refreshs may fail gracefully
    
    * typechecking in automation
    
    * PEP 621 metadata

--- condor_git_config.py
+++ condor_git_config.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""dynamically configure an HTCondor node from a git repository"""
+from typing import Union, List, Iterator, Optional, Dict, Any
+
 import sys
 import os
 import argparse
@@ -10,7 +13,9 @@ import functools
 import re
 import random
 import filelock
-import zlib
+from pathlib import Path
+
+__version__ = "0.1.2"
 
 
 try:
@@ -19,69 +24,71 @@ except OSError:
     display_columns = 80
 else:
     # fix argparse help output -- https://bugs.python.org/issue13041
-    os.environ.setdefault('COLUMNS', str(display_columns))
+    os.environ.setdefault("COLUMNS", str(display_columns))
 
 
 CLI = argparse.ArgumentParser(
-    description='Dynamic Condor Configuration Hook',
-    fromfile_prefix_chars='@',
+    description="Dynamic Condor Configuration Hook",
+    fromfile_prefix_chars="@",
     formatter_class=functools.partial(
         argparse.ArgumentDefaultsHelpFormatter,
-        max_help_position=max(24, int(0.3 * display_columns))
+        max_help_position=max(24, int(0.3 * display_columns)),
     ),
 )
-CLI_SOURCE = CLI.add_argument_group('source of configuration files')
+CLI_SOURCE = CLI.add_argument_group("source of configuration files")
 CLI_SOURCE.add_argument(
-    dest='git_uri',
-    metavar='GIT-URI',
-    help='git repository URI to fetch files from',
+    dest="git_uri",
+    metavar="GIT-URI",
+    help="git repository URI to fetch files from",
 )
 CLI_SOURCE.add_argument(
-    '-b', '--branch',
-    help='branch to fetch files from',
-    default='master',
+    "-b",
+    "--branch",
+    help="branch to fetch files from",
+    default="master",
 )
-CLI_CACHE = CLI.add_argument_group('local configuration cache')
+CLI_CACHE = CLI.add_argument_group("local configuration cache")
 CLI_CACHE.add_argument(
-    '--cache-path',
-    help='path to cache configuration file sources',
-    default='/etc/condor/config.git/',
+    "--cache-path",
+    help="path to cache configuration file sources",
+    default=Path("/etc/condor/config.git/"),
+    type=Path,
 )
 CLI_CACHE.add_argument(
-    '--max-age',
-    help='seconds before a new update is pulled',
+    "--max-age",
+    help="seconds before a new update is pulled; use inf to disable updates",
     default=300 + random.randint(-10, 10),
-    type=int,
+    type=float,
 )
-CLI_SELECTION = CLI.add_argument_group('configuration selection')
+CLI_SELECTION = CLI.add_argument_group("configuration selection")
 CLI_SELECTION.add_argument(
-    '--pattern',
-    help='regular expression(s) for configuration files',
-    nargs='*',
-    default=[r'^[^.].*\.cfg$'],
+    "--pattern",
+    help="regular expression(s) for configuration files",
+    nargs="*",
+    default=[r"^[^.].*\.cfg$"],
 )
 CLI_SELECTION.add_argument(
-    '--blacklist',
-    help='regular expression(s) for ignoring configuration files',
-    nargs='*',
+    "--blacklist",
+    help="regular expression(s) for ignoring configuration files",
+    nargs="*",
     default=[],
 )
 CLI_SELECTION.add_argument(
-    '--whitelist',
-    help='regular expression(s) for including ignored files',
-    nargs='*',
+    "--whitelist",
+    help="regular expression(s) for including ignored files",
+    nargs="*",
     default=[],
 )
 CLI_SELECTION.add_argument(
-    '--recurse',
-    help='provide files beyond the top-level',
-    action='store_true',
+    "--recurse",
+    help="provide files beyond the top-level",
+    action="store_true",
 )
-CLI_INTEGRATION = CLI.add_argument_group('configuration integration')
+CLI_INTEGRATION = CLI.add_argument_group("configuration integration")
 CLI_INTEGRATION.add_argument(
-    '--path-key',
-    help='config key exposing the cache path',
-    default='GIT_CONFIG_CACHE_PATH',
+    "--path-key",
+    help="config key exposing the cache path",
+    default="GIT_CONFIG_CACHE_PATH",
 )
 
 LOGGER = logging.getLogger()
@@ -91,127 +98,176 @@ class ConfigCache(object):
     """
     Cache for configuration files from git
     """
-    def __init__(self, git_uri, branch, cache_path, max_age):
+
+    def __init__(self, git_uri: str, branch: str, cache_path: Path, max_age: float):
         self.git_uri = git_uri
         self.branch = branch
         self.cache_path = cache_path
         self.max_age = max_age
-        self._work_path = os.path.abspath(os.path.join(cache_path, branch))
-        self._meta_file = self.abspath('cache.json')
-        self._cache_lock = filelock.FileLock(
-            os.path.join('/tmp', '.cache.%s.lock' % zlib.adler32(os.path.basename(__file__).encode()))
-        )
-        os.makedirs(self._work_path, exist_ok=True, mode=0o755)
+        self._work_path = cache_path.resolve() / branch
+        self._work_path.mkdir(mode=0o755, parents=True, exist_ok=True)
+        self._meta_file = self.abspath("cache.json")
+        self._cache_lock = filelock.FileLock(str(self.abspath(f"cache.{branch}.lock")))
+        self._config_meta: Optional[Dict[str, Any]] = None
 
-    def abspath(self, *rel_paths):
-        return os.path.abspath(os.path.join(self._work_path, *rel_paths))
+    @property
+    def stats(self):
+        """Statistics of the cache"""
+        meta = self._config_meta if self._config_meta is not None else self._read_meta()
+        age = time.time() - meta["timestamp"]
+        return {"age": age, "pulls": meta["pulls"], "reads": meta["reads"]}
 
-    def repo_path(self, *rel_paths):
-        return self.abspath('repo', *rel_paths)
+    def _read_meta(self):
+        try:
+            with open(self._meta_file, "r") as meta_stream:
+                meta_data = json.load(meta_stream)
+        except FileNotFoundError:
+            return {
+                "git_uri": self.git_uri,
+                "branch": self.branch,
+                "timestamp": 0.0,
+                "pulls": 0,
+                "reads": 0,
+            }
+        else:
+            if meta_data["git_uri"] != self.git_uri:
+                LOGGER.critical("cache %r corrupted by other hook: %r", self, meta_data)
+                raise RuntimeError(
+                    f"config cache {self.cache_path!r} used for conflicting hooks"
+                )
+            return meta_data
+
+    def _write_meta(self):
+        if self._config_meta is None:
+            return
+        with open(self._meta_file, "w") as meta_stream:
+            json.dump(self._config_meta, meta_stream)
+
+    def abspath(self, *rel_paths: Union[str, Path]) -> Path:
+        return self._work_path.joinpath(*rel_paths)
+
+    def repo_path(self, *rel_paths: Union[str, Path]) -> Path:
+        return self.abspath("repo", *rel_paths)
 
     def __enter__(self):
         self._cache_lock.acquire()
+        self._config_meta = self._read_meta()
+        self._refresh()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self._write_meta()
+        self._config_meta = None
         self._cache_lock.release()
         return False
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Path]:
+        assert self._config_meta is not None
         # avoid duplicates from links
-        seen = set()
         repo_path = self.repo_path()
-        for dir_path, dir_names, file_names in os.walk(repo_path):
-            if '.git' in dir_names:
-                dir_names.remove('.git')
-            dir_names[:] = sorted(dir_names)
+        for base_dir, dir_names, file_names in os.walk(repo_path):
+            try:
+                dir_names.remove(".git")
+            except ValueError:
+                pass
+            dir_names.sort()
+            dir_path = Path(base_dir)
             for file_name in sorted(file_names):
-                rel_path = os.path.normpath(os.path.relpath(os.path.join(dir_path, file_name), repo_path))
-                if rel_path in seen:
-                    continue
-                seen.add(rel_path)
+                rel_path = (dir_path / file_name).relative_to(repo_path)
                 yield rel_path
 
-    @property
-    def outdated(self):
-        try:
-            with open(self._meta_file, 'r') as raw_meta:
-                meta_data = json.load(raw_meta)
-        except FileNotFoundError:
-            return True
-        else:
-            if meta_data['git_uri'] != self.git_uri or meta_data['branch'] != self.branch:
-                LOGGER.critical('cache %r corrupted by other hook: %r', self, meta_data)
-                raise RuntimeError('config cache %r used for conflicting hooks' % self.cache_path)
-            else:
-                return meta_data['timestamp'] + self.max_age <= time.time()
-
-    def _update_metadata(self):
-        with open(self._meta_file, 'w') as raw_meta:
-            json.dump({'git_uri': self.git_uri, 'branch': self.branch, 'timestamp': time.time()}, raw_meta)
-
-    def refresh(self):
-        if not self.outdated:
-            return
+    def _refresh(self):
+        assert self._config_meta is not None
+        self._config_meta["reads"] += 1
+        if self._config_meta["timestamp"] + self.max_age > time.time():
+            return  # early exit as ConfigCache is still valid
         repo_path = self.repo_path()
-        if not os.path.exists(os.path.join(repo_path, '.git')):
+        self._config_meta["pulls"] += 1
+        if not self.repo_path(".git").exists():
+            branch = [] if not self.branch else ["--branch", self.branch]
             subprocess.check_output(
-                ['git', 'clone', '--quiet', '--branch', str(self.branch), str(self.git_uri), repo_path],
+                ["git", "clone", "--quiet", *branch, self.git_uri, str(repo_path)],
                 timeout=30,
                 universal_newlines=True,
             )
+            self._config_meta["timestamp"] = time.time()
         else:
-            subprocess.check_output(
-                ['git', 'pull'],
-                timeout=30,
-                cwd=repo_path,
-                universal_newlines=True
-            )
-        self._update_metadata()
+            try:
+                subprocess.check_output(
+                    ["git", "pull"], timeout=30, cwd=repo_path, universal_newlines=True
+                )
+            except (subprocess.CalledProcessError, subprocess.TimeoutExpired):
+                pass
+            else:
+                self._config_meta["timestamp"] = time.time()
 
 
 class ConfigSelector(object):
-    def __init__(self, pattern, blacklist, whitelist, recurse):
+    """
+    Selector for a configuration file iterator
+    """
+
+    def __init__(
+        self,
+        pattern: List[str],
+        blacklist: List[str],
+        whitelist: List[str],
+        recurse: bool,
+    ):
         self.pattern = self._prepare_re(pattern)
-        self.blacklist = self._prepare_re(blacklist, '(?!)')
+        self.blacklist = self._prepare_re(blacklist, default="(?!)")
         self.whitelist = self._prepare_re(whitelist)
         self.recurse = recurse
 
     @staticmethod
-    def _prepare_re(pieces, default='.*'):
+    def _prepare_re(pieces, default=".*") -> "re.Pattern":
         if not pieces:
             return re.compile(default)
         if len(pieces) == 1:
             return re.compile(pieces[0])
         else:
-            return re.compile('|'.join('(?:%s)' % piece for piece in pieces))
+            return re.compile("|".join(f"(?:{piece})" for piece in pieces))
 
-    def get_paths(self, config_cache):
+    def get_paths(self, config_cache: ConfigCache) -> Iterator[Path]:
+        pattern, blacklist, whitelist = self.pattern, self.blacklist, self.whitelist
         for rel_path in config_cache:
-            if not self.recurse and os.path.dirname(rel_path):
+            if not self.recurse and rel_path.parent != Path("."):
                 continue
-            if self.pattern.search(rel_path):
-                if not self.blacklist.search(rel_path) or self.whitelist.search(rel_path):
+            str_path = str(rel_path)
+            if pattern.search(str_path):
+                if not blacklist.search(str_path) or whitelist.search(str_path):
                     yield config_cache.repo_path(rel_path)
 
 
-def include_configs(path_key, config_cache, config_selector, destination=sys.stdout):
+def include_configs(
+    path_key: str,
+    config_cache: ConfigCache,
+    config_selector: ConfigSelector,
+    destination=sys.stdout,
+):
     with config_cache:
-        config_cache.refresh()
-        print('%s = %s' % (path_key, config_cache.repo_path()), file=destination)
+        print("#", json.dumps(config_cache.stats), file=destination)
+        print(f"{path_key} = {config_cache.repo_path()}", file=destination)
         for config_path in config_selector.get_paths(config_cache):
-            print('include : %s' % config_path, file=destination)
+            print(f"include : {config_path}", file=destination)
 
 
 def main():
     options = CLI.parse_args()
     config_cache = ConfigCache(
-        git_uri=options.git_uri, branch=options.branch, cache_path=options.cache_path, max_age=options.max_age
+        git_uri=options.git_uri,
+        branch=options.branch,
+        cache_path=options.cache_path,
+        max_age=options.max_age,
     )
     config_selector = ConfigSelector(
-        pattern=options.pattern, blacklist=options.blacklist, whitelist=options.whitelist, recurse=options.recurse
+        pattern=options.pattern,
+        blacklist=options.blacklist,
+        whitelist=options.whitelist,
+        recurse=options.recurse,
     )
     include_configs(options.path_key, config_cache, config_selector)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tests/integration/test_cache.py
+++ tests/integration/test_cache.py
@@ -3,6 +3,7 @@ import tempfile
 import shutil
 import pathlib
 import sys
+import json
 
 
 REPO_URL = "https://github.com/MatterMiners/condor-git-config.git"
@@ -10,31 +11,58 @@ EXECUTABLE = [shutil.which("condor-git-config")]
 if EXECUTABLE[0] is None:
     EXECUTABLE = [
         sys.executable,
-        str(pathlib.Path(__file__).parent.parent.parent / "condor_git_config.py")
+        str(pathlib.Path(__file__).parent.parent.parent / "condor_git_config.py"),
     ]
 
 
 def test_checkout():
     with tempfile.TemporaryDirectory() as cache:
-        subprocess.check_call(
-            [*EXECUTABLE, REPO_URL, '--cache-path', cache]
-        )
-        subprocess.check_call(
-            [*EXECUTABLE, REPO_URL, '--cache-path', cache]
-        )
+        subprocess.check_call([*EXECUTABLE, REPO_URL, "--cache-path", cache])
+        subprocess.check_call([*EXECUTABLE, REPO_URL, "--cache-path", cache])
 
 
 def test_includes():
     with tempfile.TemporaryDirectory() as cache:
         command = [
-            *EXECUTABLE, REPO_URL, '--cache-path', cache, '--pattern', r'.*\.py', '--path-key', 'CHECKOUT_ROOT'
+            *EXECUTABLE,
+            REPO_URL,
+            "--cache-path",
+            cache,
+            "--pattern",
+            r".*\.py",
+            "--path-key",
+            "CHECKOUT_ROOT",
         ]
-        initial = subprocess.check_output(command)
-        refresh = subprocess.check_output(command)
-        assert initial == refresh
-        assert b"CHECKOUT_ROOT" in initial
+        initial = subprocess.check_output(command).splitlines()
+        refresh = subprocess.check_output(command).splitlines()
+        assert initial[:1] != refresh[:1]
+        assert initial[1:] == refresh[1:]
+        assert b"CHECKOUT_ROOT" in b"\n".join(initial)
         root = next(
-            line.partition(b"=")[-1].strip() for line in initial.splitlines() if b"CHECKOUT_ROOT" in line
+            line.partition(b"=")[-1].strip()
+            for line in initial[1:]
+            if b"CHECKOUT_ROOT" in line
         )
         expected_include = b"include : %s/condor_git_config.py" % root
-        assert expected_include in initial.splitlines()
+        assert expected_include in initial[1:]
+
+
+def test_refresh():
+    with tempfile.TemporaryDirectory() as cache:
+        command = [
+            *EXECUTABLE,
+            REPO_URL,
+            "--cache-path",
+            cache,
+            "--pattern",
+            r".*\.py",
+            "--path-key",
+            "CHECKOUT_ROOT",
+        ]
+        previous_meta = json.loads(subprocess.check_output(command).splitlines()[0][1:])
+        for _ in range(5):
+            new_meta = json.loads(subprocess.check_output(command).splitlines()[0][1:])
+            assert new_meta["age"] > previous_meta["age"]
+            assert new_meta["reads"] == previous_meta["reads"] + 1
+            assert new_meta["pulls"] == previous_meta["pulls"]
+            previous_meta = new_meta
